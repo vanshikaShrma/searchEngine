c422592f125d5ad5de33d31a1b1967850f50bd26
vanshikaShrma <sharmavanshika3818@gmail.com>
2025-08-01 09:56:49 +0530

    added new func

--- LinkedListHeader.h
+++ LinkedListHeader.h
@@ -1,69 +1,103 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 template <typename T>
-class Node{
-    public : 
+class Node
+{
+public:
     T val;
-    Node* next;
-    //Constructors
-    Node(T val) {
-        this -> val = val;
-        this -> next = NULL;
+    Node *next;
+    // Constructors
+    Node(T val)
+    {
+        this->val = val;
+        this->next = NULL;
     }
 };
 
-template<typename T>
-class List {
-    Node<T>* head = NULL;
-    public : 
-    //add Node
+template <typename T>
+class List
+{
+    Node<T> *head = NULL;
+
+public:
+    ~List()
+    {
+        Node<T> *temp;
+        while (head)
+        {
+            temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+    // add Node
     void addAtStart(T data);
     void addInMiddle(int index, T data);
     void addAtLast(T data);
 
-    //display list
+    // display list
     void display();
-
 };
 
-template<typename T>
-void List<T>::addAtStart(T data) {
-    Node<T>* newNode = new Node<T>(data);
-    newNode->next = head; 
-    head = newNode;     
+template <typename T>
+void List<T>::addAtStart(T data)
+{
+    Node<T> *newNode = new Node<T>(data);
+    newNode->next = head;
+    head = newNode;
 }
 
-//add after the given index (0 indexed)
-template<typename T>
-void List<T> :: addInMiddle(int index, T data){
-    Node<T>* temp = head;
-    Node<T>* tail = NULL;
-    Node<T>* newNode = new Node<T>(data);
-    int i=0;
-    while(temp && i <= index){
+// add after the given index (0 indexed)
+template <typename T>
+void List<T>::addInMiddle(int index, T data)
+{
+    Node<T> *temp = head;
+    Node<T> *tail = NULL;
+    Node<T> *newNode = new Node<T>(data);
+    int i = 0;
+    while (temp && i <= index)
+    {
         tail = temp;
-        temp = temp -> next;
+        temp = temp->next;
         i++;
     }
 
-    if(temp == NULL){
+    if (temp == NULL)
+    {
         head = newNode;
     }
-    else {
+    else
+    {
         tail.next = newNode;
     }
 }
 
-template<typename T>
-void List<T> :: addAtLast(T data){
+template <typename T>
+void List<T>::addAtLast(T data)
+{
+    Node<T> *newNode = new Node<T>(data);
 
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+
+    Node<T> *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
 }
 
-template<typename T>
-void List<T> :: display(){
-    Node<T>* temp = head;
-    while(temp){
-        cout<<(temp -> val)<<"\n";
-        temp = temp -> next;
+template <typename T>
+void List<T>::display()
+{
+    Node<T> *temp = head;
+    while (temp)
+    {
+        cout << (temp->val) << "\n";
+        temp = temp->next;
     }
 }
