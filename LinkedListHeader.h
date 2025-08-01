65fe27654edbf0c1597e4bac35aca22ceea765f2
vanshikaShrma <sharmavanshika3818@gmail.com>
2025-08-01 12:38:26 +0530

    change

--- LinkedListHeader.h
+++ LinkedListHeader.h
@@ -34,6 +34,9 @@ public:
     void addAtStart(T data);
     void addInMiddle(int index, T data);
     void addAtLast(T data);
+    Node<T> *deleteHead();
+    Node<T> *deleteatIndex(int index); // 0 indexed
+    Node<T> *deleteFromLast();
 
     // display list
     void display();
@@ -100,4 +103,74 @@ void List<T>::display()
         cout << (temp->val) << "\n";
         temp = temp->next;
     }
+}
+template <typename T>
+Node<T> *List<T>::deleteHead()
+{
+    if (head == NULL)
+    {
+        cout << "List undeflow\n";
+        return NULL;
+    }
+    Node<T> *temp = head;
+    head = head->next;
+    return temp;
+}
+
+template <typename T>
+Node<T> *List<T>::deleteFromLast()
+{
+    if (head == NULL)
+    {
+        cout << "List Underflow\n";
+        return NULL;
+    }
+    if (head->next == NULL)
+    {
+        Node<T> *temp = head;
+        head = NULL;
+        return head;
+    }
+    Node<T> *temp = head;
+    Node<T> *tail = NULL;
+    while (temp->next)
+    {
+        tail = temp;
+        temp = temp->next;
+    }
+    tail->next = NULL;
+    return temp;
+}
+
+template <typename T>
+Node<T> *List<T>::deleteatIndex(int index)
+{
+    int i = 0;
+    Node<T> *temp = head;
+    if (head == NULL)
+    {
+        cout << "List Underflow";
+        return NULL;
+    }
+    if (index == 0)
+    {
+        Node<T> *t = head;
+        head = head->next;
+        return t;
+    }
+    Node<T> *tail = NULL;
+    while (temp && i < index)
+    {
+        tail = temp;
+        temp = temp->next;
+        i++;
+    }
+    if (temp == NULL)
+    {
+        cout << "NO node exist";
+        return NULL;
+    }
+    Node<T> *t = temp;
+    tail->next = temp->next;
+    return t;
 }
